4280cf4f71cd7fc6153ca0a4f8d1bcc1fc3254d4
autumn-ragland <raglanda998@gmail.com>
2019-05-29 14:33:50 -0500

    3pm check in

--- travelProject/travelApp/views.py
+++ travelProject/travelApp/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 # import googlemaps
+# todo: python is not recognizing googlemaps module even though I can see it on pip list
+
+# gmaps = googlemaps.Client(key='Add Your Key here')
+# find a way to securely pass my ley so I don't get charged
 
 
 def index(request):
+    # todo: probably shouldn't render map like this (doesn't matter till I can import module)
     # map = new google.maps.Map
     # ({
     #     center: {lat: -35.1495, lng: 90.0490},
