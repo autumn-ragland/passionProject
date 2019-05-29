eec512d888300971116f2abe6e0877a15863879b
autumn-ragland <raglanda998@gmail.com>
2019-05-29 10:29:38 -0500

    add routes

--- travelProject/travelApp/urls.py
+++ travelProject/travelApp/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     # landing page route
     path('', views.index, name='index'),
+    path('location_details/', views.location_details, name='location_details'),
+    path('log_details/', views.log_details, name='log_details'),
 ]
--- travelProject/travelApp/views.py
+++ travelProject/travelApp/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'travelApp/index.html')
+
+
+def location_details(request):
+    return render(request, 'travelApp/locationDetails.html')
+
+
+def log_details(request):
+    return render(request, 'travelApp/logDetails.html')
