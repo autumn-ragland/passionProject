b258b6bfea34b37913ac24fed31b6787d40269d5
autumn-ragland <raglanda998@gmail.com>
2019-05-30 08:49:27 -0500

    9am check in

--- travelProject/travelApp/views.py
+++ travelProject/travelApp/views.py
@@ -1,22 +1,26 @@
 from django.shortcuts import render
 # import googlemaps
+# from .mapsGitHub import Client
 # todo: python is not recognizing googlemaps module even though I can see it on pip list
 
-# gmaps = googlemaps.Client(key='Add Your Key here')
-# find a way to securely pass my ley so I don't get charged
-
+# gmaps = googlemaps.Client(key='AIzaSyAcVeR1EcysjeZr4eQE_mtiJ6suMxXY52Y')
+# find a way to securely pass my key so I don't get charged
 
 def index(request):
     # todo: probably shouldn't render map like this (doesn't matter till I can import module)
-    # map = new google.maps.Map
-    # ({
-    #     center: {lat: -35.1495, lng: 90.0490},
+    # let embedMap = new google.maps.Map{
+    #     center: {lat: -34.397, lng: 150.644},
     #     zoom: 8
-    # })
+    # }
     context = {
-        'map': map
+        # 'map': embedMap
     }
     return render(request, 'travelApp/index.html', context)
+
+
+# def test():
+#     geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+#     print(geocode_result)
 
 
 def location_details(request):
